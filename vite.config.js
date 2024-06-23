6c1f32cba2aa1721e8f803f9ddf1be50d6f00a18
CAPOOCAT-LAPTOP\trant <tranthaitat99@gmail.com>
2024-06-23 22:33:16 +0700

    #fixbug => fix compress error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), compression({ deleteOriginalAssets: true }), tarball()],
+  plugins: [react(), compression({ algorithm: "brotliCompress" }), tarball()],
 });
