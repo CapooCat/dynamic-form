1f699308bcda0e99efdac1fcfdd206588eada9a1
CAPOOCAT-LAPTOP\trant <tranthaitat99@gmail.com>
2024-06-23 22:29:22 +0700

    #optimization => compress only keep .gz

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), compression(), tarball()],
+  plugins: [react(), compression({ deleteOriginalAssets: true }), tarball()],
 });
